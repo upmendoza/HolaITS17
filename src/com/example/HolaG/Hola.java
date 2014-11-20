0178dd089c585ddf9b396b78945fb1e13ea00c14
Jesus Alberto Villa Villa <villavilla_alberto@hotmail.com>
2014-11-20 12:06:28 -0700

    Boton de Villa
    Añadí mi boton

--- src/com/example/HolaG/Hola.java
+++ src/com/example/HolaG/Hola.java
@@ -2,6 +2,8 @@ package com.example.HolaG;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class Hola extends Activity
@@ -13,5 +15,14 @@ public class Hola extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         Toast.makeText(this.getApplicationContext(),"Mi Texto para Mostrar",Toast.LENGTH_LONG).show();
-    }
+
+        Button alberto = (Button) findViewById(R.id.villa);
+            alberto.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getApplicationContext(),"Alberto villa",Toast.LENGTH_LONG).show();
+                }
+            });
+        }
 }
+
