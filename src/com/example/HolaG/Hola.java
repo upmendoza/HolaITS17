f3cdf087b5127c58bf2f6e8a507600f3fc1b6e38
jorgeurqdzo <jorgedzo_17@hotmail.com>
2015-11-13 19:38:24 -0700

    Se cambio el nombre de la app en Strings, Se agrego un layout para login, Se limpio clase hola y layout main.xml

--- src/com/example/HolaG/Hola.java
+++ src/com/example/HolaG/Hola.java
@@ -24,46 +24,7 @@ public class Hola extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        Toast.makeText(this.getApplicationContext(),"Mi Texto para Mostrar",Toast.LENGTH_LONG).show();
+        setContentView(R.layout.login);
 
-        Button alberto = (Button) findViewById(R.id.villa);
-            alberto.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(getApplicationContext(),"Alberto villa",Toast.LENGTH_LONG).show();
-                }
-            });
-        
-
-        Button Fito = (Button)findViewById(R.id.btnfito);
-
-        Fito.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Toast.makeText(getApplicationContext(),"Rodolfo :D :P",Toast.LENGTH_LONG).show();
-            }
-
-        });
-
-        Button bjavier = (Button) findViewById(R.id.button_javier);
-
-        bjavier.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Button Javier",Toast.LENGTH_LONG).show();
-            }
-        });
-
-        Button bdulce = (Button) findViewById(R.id.button_dulce);
-
-        bdulce.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Button Dulce :DD",Toast.LENGTH_LONG).show();
-            }
-        });
     }
 }
