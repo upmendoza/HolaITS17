4ce2af2bb8a4fe35cc72ab54e5a3207d741221fa
jorgeurqdzo <jorgedzo_17@hotmail.com>
2015-11-13 19:58:54 -0700

    Validacion con datos estaticos de usuario y contraseña

--- src/com/example/HolaG/Hola.java
+++ src/com/example/HolaG/Hola.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class Hola extends Activity
@@ -20,11 +21,30 @@ public class Hola extends Activity
     /*JUAN MARTINEZ*/
     /*JorshxD was here*/
 
+    private Button iniciar;
+    private EditText user,pass;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        user = (EditText) findViewById(R.id.etUsuario);
+        pass = (EditText) findViewById(R.id.etPass);
+
+        iniciar = (Button) findViewById(R.id.btIniciar);
+
+        iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(user.getText().toString().equals("jorge") && pass.getText().toString().equals("123"))
+                    Toast.makeText(getApplication(),"Login Correcto",Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getApplication(),"Datos Erroneos",Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
